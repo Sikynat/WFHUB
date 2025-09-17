63d6ea74ee7249c140e8e50a0f8e1eb6a33c341d
Caique Dadonas <caiquedadonas@outlook.com>
2025-09-17 20:37:17 -0300

    Foreing Key

--- wefixhub/admin.py
+++ wefixhub/admin.py
@@ -3,7 +3,7 @@ from wefixhub.models import WfClient
 # Register your models here.
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ['client_id', 'client_code', 'client_name', 'client_cnpj', 'client_adress', 'client_city', 'client_state', 'client_state_subscription', 'client_date', 'is_active']
-    search_fields =  ['client_id', 'client_code', 'client_name', 'client_cnpj', 'client_adress', 'client_city', 'client_state', 'client_state_subscription', 'client_date', 'is_active']
+    list_display = ['client_id', 'client_code', 'client_name', 'client_cnpj', 'client_adress', 'client_city', 'client_state', 'client_state_subscription', 'client_date' ]
+    search_fields =  ['client_id', 'client_code', 'client_name', 'client_cnpj', 'client_adress', 'client_city', 'client_state', 'client_state_subscription', 'client_date',]
 
 admin.site.register(WfClient, ClientAdmin)
--- wefixhub/models.py
+++ wefixhub/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
+class UF(models.Model):
+    uf_id = models.AutoField(primary_key=True)
+    uf_name = models.CharField(max_length=2)
+
+
 
 class WfClient(models.Model):
     client_id = models.AutoField(primary_key=True)
@@ -11,10 +15,10 @@ class WfClient(models.Model):
     client_cnpj = models.CharField(max_length=14, unique=True)
     client_adress = models.CharField(max_length=255)
     client_city = models.CharField(max_length=100)
-    client_state = models.CharField(max_length=2)
+    client_state = models.ForeignKey(UF,on_delete=models.PROTECT, related_name='state_uf')
     client_state_subscription = models.CharField(max_length=14, blank=True, null=True)
     client_date = models.DateField(blank=True, null=True)
-    is_active = models.BooleanField(default=False)
+    client_is_active = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'wf_client'
