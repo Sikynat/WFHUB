079147f2fcf14fe3bc679124da0603c8d8365ae8
Caique Dadonas <caiquedadonas@MacBook-Air-de-Caique.local>
2025-10-01 21:07:47 -0300

    Edit client admin painel

--- wefixhub/admin.py
+++ wefixhub/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.utils.html import format_html
 from .models import WfClient, Product, Pedido, ItemPedido, wefixhub_uf, Endereco
 
 # Admin personalizado para WfClient
@@ -12,6 +15,9 @@ class ClientAdmin(admin.ModelAdmin):
     
     # Campos de busca
     search_fields = ['client_name', 'client_code', 'client_cnpj']
+    
+    # Adiciona a ação de edição
+    actions = ['editar_cliente']
 
     def save_model(self, request, obj, form, change):
         # Se for um novo cliente (não uma edição)
@@ -27,6 +33,18 @@ class ClientAdmin(admin.ModelAdmin):
             
         # Salva o objeto no banco de dados
         super().save_model(request, obj, form, change)
+    
+    def editar_cliente(self, request, queryset):
+        # Ação que redireciona para a página de edição de um único item
+        if queryset.count() == 1:
+            obj = queryset.first()
+            # Esta linha gera a URL para a página de edição do objeto no admin
+            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[obj.pk])
+            return redirect(url)
+        else:
+            self.message_user(request, "Por favor, selecione apenas um cliente para editar.", level='WARNING')
+
+    editar_cliente.short_description = "Editar cliente selecionado"
 
 # Admin para Product (declaração única e correta)
 class ProductAdmin(admin.ModelAdmin):
